857ee858071afb038a5aeb433615f4e250a85c79
mwiesiolek <maksymilian.wiesiolek@gmail.com>
2017-11-04 16:24:56 +0100

    drop down list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
   isChecked = true;
+  items = [
+    {id: 1, title: 'item1'},
+    {id: 2, title: 'item2'},
+    {id: 3, title: 'item3'},
+  ];
+  color = 2;
 
   onChange($event) {
     console.log($event);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import {BrowserModule} from "@angular/platform-browser";
 import {NgModule} from "@angular/core";
 import {AppComponent} from "./app.component";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {MatCheckboxModule, MatRadioModule} from "@angular/material";
+import {MatCheckboxModule, MatRadioModule, MatSelectModule} from "@angular/material";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -12,7 +13,9 @@ import {MatCheckboxModule, MatRadioModule} from "@angular/material";
     BrowserModule,
     BrowserAnimationsModule,
     MatCheckboxModule,
-    MatRadioModule
+    MatRadioModule,
+    MatSelectModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
