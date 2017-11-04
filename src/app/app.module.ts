e8bb9661c32a1593360299de23e2f0a2eb762dc3
mwiesiolek <maksymilian.wiesiolek@gmail.com>
2017-11-04 16:17:03 +0100

    simple radio button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import {BrowserModule} from "@angular/platform-browser";
 import {NgModule} from "@angular/core";
 import {AppComponent} from "./app.component";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {MatCheckboxModule} from "@angular/material";
+import {MatCheckboxModule, MatRadioModule} from "@angular/material";
 
 @NgModule({
   declarations: [
@@ -11,7 +11,8 @@ import {MatCheckboxModule} from "@angular/material";
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatRadioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
